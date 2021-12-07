cdf65af5fd064b760782cb442c2a5dce7b1c44bb
AD <andreadonadios@gmail.com>
2021-12-07 16:33:11 +0100

    fixed settings.py

--- caccia/settings.py
+++ caccia/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,7 +75,7 @@ WSGI_APPLICATION = 'caccia.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
